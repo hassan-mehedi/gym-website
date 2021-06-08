46d83839e07e927af965122993d72188e42addae
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2021-06-09 01:37:00 +0600

    Benefits Section Added With fully responsiveness for different max widths, One feature added for the navbar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
+// Menu bar toggle
+
 const toggleButton = document.querySelector(".toggle-menu");
 const menuBar = document.querySelector(".menu-bar");
 
 toggleButton.addEventListener("click", () => {
     menuBar.classList.toggle("active-menu-bar");
     toggleButton.classList.toggle("active");
+});
+
+// Change Navbar Color while scrolling
+
+const navBar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+    let y = window.pageYOffset;
+    y = parseInt(y);
+    if (y > 30) {
+        navBar.style.backgroundColor = "#efefef";
+    }
+    if (y <= 30) {
+        navBar.style.backgroundColor = "rgba(239,239,239, 0.5)";
+    }
+    console.log(y);
 });
